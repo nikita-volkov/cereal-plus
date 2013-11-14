0785bd5e31d8bb9bab99de2ef0e1d004d70bdcea
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-11-14 15:40:01 +0400

    All around SerializableM

--- src/CerealPlus.hs
+++ src/CerealPlus.hs
@@ -1,5 +1,35 @@
-module CerealPlus where
+module CerealPlus 
+  (
+    SerializableM(..),
+    serialize,
+    deserialize,
+    runSerializeT,
+    runDeserializeT,
+  ) where
 
 import CerealPlus.Prelude
 import qualified CerealPlus.SerializeT as SerializeT; import CerealPlus.SerializeT (Serialize, SerializeT)
 import qualified CerealPlus.DeserializeT as DeserializeT; import CerealPlus.DeserializeT (Deserialize, DeserializeT)
+import CerealPlus.SerializableM
+
+
+serialize :: (SerializableM a Identity) => a -> Serialize ()
+serialize = serializeT
+
+deserialize :: (SerializableM a Identity) => Deserialize a
+deserialize = deserializeT
+
+runSerializeT = SerializeT.run
+runDeserializeT = DeserializeT.run
+
+
+-- TODO
+
+-- runSerialize = runIdentity . SerializeT.run
+
+-- runDeserialize deserialize = runIdentity . DeserializeT.run deserialize
+
+
+
+
+
