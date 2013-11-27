6c5b21a0b4058978fef8bb9d1c910f8b52808fbf
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-11-27 21:20:19 +0400

    MonadCIO and MonadSTM instances for Serialize

--- src/CerealPlus/Prelude.hs
+++ src/CerealPlus/Prelude.hs
@@ -96,6 +96,9 @@ import Data.Time.Clock.TAI as Exports (AbsoluteTime)
 -- errors
 import Control.Error as Exports
 
+-- cio
+import CIO as Exports
+
 import qualified Data.ByteString.Lazy
 import qualified Data.Text.Lazy
 import qualified Data.Vector.Primitive
